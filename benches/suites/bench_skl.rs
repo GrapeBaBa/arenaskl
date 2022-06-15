b94d975d7370ee46825a66853ca18dc104232eb2
grapebaba <281165273@qq.com>
2022-06-15 18:33:20 +0800

    Add skl bench
    Signed-off-by: grapebaba <281165273@qq.com>

--- benches/suites/bench_skl.rs
+++ benches/suites/bench_skl.rs
@@ -16,5 +16,19 @@ fn criterion_benchmark_skl_add(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark_skl_add);
+fn criterion_benchmark_skl_iter(c: &mut Criterion) {
+    let mut a = Arena::new(u32::MAX);
+    let mut skl = Skiplist::new(&mut a).unwrap();
+    let key = InternalKey::new(&[1u8, 1u8, 1u8, 1u8], 1u64, INTERNAL_KEY_KIND_SET);
+    let _ = skl.add(&key, &[1u8, 1u8, 1u8, 1u8]);
+
+    c.bench_function("skl_iter", |b| {
+        b.iter(|| {
+            let mut iter = skl.iter(&[] as &[u8], &[] as &[u8]);
+            iter.next();
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark_skl_add,criterion_benchmark_skl_iter);
 criterion_main!(benches);
